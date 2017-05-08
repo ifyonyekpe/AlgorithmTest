572f50f865518bcf0c08ff5a7ad4f0de373b9cc9
ifyonyekpe <ifyonyekpe@gmail.com>
2017-05-07 22:35:47 -0500

    initial commit

---
+++ AlgorithmTest/ForLoop.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmTest
+{
+    public class ForLoop
+    {
+        public void First_Test(List<Plan> inputs)
+        {
+            for (int i = 0; i <= inputs.Count; i++)
+            {
+                for (int j = 0; j <= inputs[i].Features.Count; i++)
+                {
+
+                }
+            }
+        }
+
+        public void Second_Test(List<Plan> inputs)
+        {
+            foreach(var i in inputs)
+            {
+                foreach (var j in i.Features)
+                {
+
+                }
+            }
+        }
+
+        public void Third_Test(List<Plan> inputs)
+        {
+            
+        }
+    }
+
+    public class Plan
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public List<Feature> Features { get; set; }
+    }
+
+    public class Feature
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int BenefitId { get; set; }
+    }
+}
---
+++ AlgorithmTest/Helper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgorithmTest
+{
+    public static class Helper
+    {
+        private static Random random = new Random();
+        public static string GenerateValue(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, length).Select(c => c[random.Next(c.Length)]).ToArray());
+        }
+    }
+}
